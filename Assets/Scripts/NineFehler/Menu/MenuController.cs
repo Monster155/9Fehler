5adfabb9e4d2ca9d74a1939fd9ead3f3cd0738f7
damir <damir180501@mail.ru>
2023-10-28 17:47:56 +0300

    + TMP + MenuScene

---
+++ Assets/Scripts/NineFehler/Menu/MenuController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private Button _startGameButton;
+    
+    void Start()
+    {
+        _startGameButton.onClick.AddListener(StartGameButton_OnClick);
+    }
+
+    private void StartGameButton_OnClick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
